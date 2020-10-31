e399dd4169f805d7e893bfd9d2bf990a73523f63
Nite <solitude.loner7@gmail.com>
2020-10-31 21:19:41 +0700

    [Update] sumAll solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sum = (from, to) => {
+    let result = 0;
+    for ( let i = from; i <= to; i++ ) {
+        result += i;
+    }
+    return result;
+}
 
+const sumAll = function(from, to) {
+    if (typeof from != "number" 
+    || typeof to != "number" 
+    || from < 0 
+    || to < 0
+    ) {
+        return "ERROR";
+    }
+
+    if ( to > from ) {
+        return sum(from, to);
+    } else {
+        return sum(to, from);
+    }
 }
 
 module.exports = sumAll
