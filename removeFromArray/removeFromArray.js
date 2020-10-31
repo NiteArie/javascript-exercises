3fe0540d460294cb09e2a13b2f58128fd2d974dc
Nite <solitude.loner7@gmail.com>
2020-10-31 21:14:28 +0700

    [Update] removeFromArray solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...value) {
+    let result = []
+    for ( let i = 0 ; i < array.length; i++ ) {
+        if ( value.indexOf(array[i]) == -1) {
+            result.push(array[i]);
+        }
+    }
+    return result;
 }
 
 module.exports = removeFromArray
