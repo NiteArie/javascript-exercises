6f7bb158f1209b2337c4941740e486571763e712
Nite <solitude.loner7@gmail.com>
2020-10-31 21:04:26 +0700

    [Update] reverseString solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let result = '';
+    for ( let i = text.length; i >= 0; i-- ) {
+        result += text.charAt(i);
+    }
+    return result;
 }
 
 module.exports = reverseString
