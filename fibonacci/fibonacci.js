c4abfd963773d488b7b0f4609e53b5d393fae6c1
Nite <solitude.loner7@gmail.com>
2020-11-01 12:24:37 +0700

    [Update] Fibonacci solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if ( number < -1) {
+        return "OOPS";
+    }
+    if ( number <= 1) {
+        return Number(number);
+    } else {
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
 }
 
 module.exports = fibonacci
