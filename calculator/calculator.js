d4582958889e3a650507aa9d6a52a0596e5b951d
Nite <solitude.loner7@gmail.com>
2020-11-01 11:17:00 +0700

    [Update] calculator solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add (first, second) {
+	return first + second;
 }
 
-function subtract () {
-	
+function subtract (first , second) {
+	return first - second;
 }
 
-function sum () {
-	
+function sum (numbers) {
+	let result = 0;
+	for (let i = 0; i < numbers.length; i++ ) {
+		result += numbers[i]
+	}
+	return result;
 }
 
-function multiply () {
-	
+function multiply (numbers) {
+	let result = 1;
+	for ( let i = 0; i < numbers.length; i++ ) {
+		result *= numbers[i];
+	}
+	return result;
 }
 
-function power() {
-	
+function power(number, power) {
+	let result = 1;
+	for ( let i = 0; i < power; i++ ) {
+		result *= number;
+	}
+	return result;
 }
 
-function factorial() {
-	
+function factorial(number) {
+	let result = 1;
+	for ( let i = 1; i <= number; i++) {
+		result *= i;
+	}
+	return result;
 }
 
 module.exports = {
