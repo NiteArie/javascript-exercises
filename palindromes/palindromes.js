ceb4734fa86688982e4cf750144731eb883892a3
Nite <solitude.loner7@gmail.com>
2020-11-01 11:34:50 +0700

    [Update] palindrome solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function() {
-
+const palindromes = function(text) {
+    let formatted = text.toLowerCase().replace(/[^A-Za-z]/g, "");
+    return formatted == formatted.split("").reverse().join("");
 }
 
 module.exports = palindromes
