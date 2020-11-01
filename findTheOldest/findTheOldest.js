77fc4daf61aebaefc26de18576e1ac93e0868900
Nite <solitude.loner7@gmail.com>
2020-11-01 12:48:48 +0700

    [Update] findTheOldest solution

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    people.sort((first, second) => {
+        if ( !first.yearOfDeath ) 
+            first.yearOfDeath = new Date().getFullYear();
+        if ( !second.yearOfDeath ) 
+            second.yearOfDeath = new Date().getFullYear();
+        return (first.yearOfDeath - first.yearOfBirth) - (second.yearOfDeath - second.yearOfBirth);
+    })
 
+    return people[people.length - 1];
 }
 
 module.exports = findTheOldest
